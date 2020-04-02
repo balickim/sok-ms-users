fdbe6cb98f3f9fb1d0598de98d819bb5d62e5304
balickim <stiekerosiem@gmail.com>
2020-04-02 18:35:48 +0200

    add new endpoint for group search and update basic users endpoint with limited and offset data

--- api/user.js
+++ api/user.js
@@ -4,9 +4,21 @@ const UserTable = require('./userTable.js');
 const router = new Router();
 
 router.get('/', (req, res, next) => {
-        return UserTable.getUsers()
-        .then((users) => { res.json({ users }) })
-        .catch(error => next(error));
+        let limit = req.query.limit;
+        let from = req.query.from;
+        let groupid = req.query.groupid;
+
+        UserTable.getUsers({ howMany: limit, fromRecord: from, groupid: groupid })
+                .then((users) => { res.json({ users }) })
+                .catch(error => next(error));
+});
+
+router.get('/:groupid', (req, res, next) => {
+        const { groupid } = req.params;
+
+        UserTable.getUsersByGroup({ groupid })
+                .then((users) => { res.json({ users }) })
+                .catch(error => next(error));
 });
 
 module.exports = router;
--- api/userTable.js
+++ api/userTable.js
@@ -1,10 +1,25 @@
 const pool = require('../databasePool');
 
 class UserTable {
-    static getUsers() {
+    static getUsers({ howMany, fromRecord }) {
         return new Promise((resolve, reject) => {
             pool.query(
-                `SELECT * from users`,
+                `SELECT * from users LIMIT $1 OFFSET $2`,
+                [howMany, fromRecord],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve(response.rows);
+                }
+            )
+        });
+    }
+
+    static getUsersByGroup({ groupid }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `SELECT * from users where groupid = $1`,
+                [groupid],
                 (error, response) => {
                     if (error) return reject(error);
 
