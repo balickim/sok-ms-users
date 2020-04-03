1101d4065352ee3c6d477fa8504c408901aa5441
balickim <stiekerosiem@gmail.com>
2020-04-03 13:36:20 +0200

    add 2 new endpoints

--- api/user.js
+++ api/user.js
@@ -3,7 +3,23 @@ const UserTable = require('./userTable.js');
 
 const router = new Router();
 
+router.post('/', (req, res, next) => {
+        let { name, surname, groupid } = req.body;
+
+        UserTable.addUser(name, surname, groupid)
+                .then(() => res.json({ message: 'dodano' }))
+                .catch(error => next(error));
+});
+
 router.get('/', (req, res, next) => {
+        let { name, surname, groupid } = req.body;
+
+        UserTable.getUser(name, surname, groupid)
+                .then((user) => { res.json({ user }) })
+                .catch(error => next(error));
+});
+
+router.get('/list', (req, res, next) => {
         let limit = req.query.limit;
         let from = req.query.from;
         let groupid = req.query.groupid;
--- api/userTable.js
+++ api/userTable.js
@@ -1,6 +1,35 @@
 const pool = require('../databasePool');
 
 class UserTable {
+    static addUser(name, surname, groupid) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `insert into users (name, surname, groupid) values ($1, $2, $3);`,
+                [name, surname, groupid],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve();
+                }
+            )
+        });
+    }
+
+    static getUser(name, surname, groupid) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `SELECT * from users 
+                where name = $1 AND surname = $2 AND groupid = $3`,
+                [name, surname, groupid],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve(response.rows);
+                }
+            )
+        });
+    }
+
     static getUsers({ howMany, fromRecord }) {
         return new Promise((resolve, reject) => {
             pool.query(
